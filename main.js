8e0d4e15cadb94d42a833e172f20fb27455509f3
mahguerra08 <129458082+mahguerra08@users.noreply.github.com>
2024-05-29 21:20:46 -0300

    Add files via upload
    aa

---
+++ main.js
@@ -0,0 +1,14 @@
+const botoes = document.querySelectorAll(".botao");
+for(let i=0;i <botoes.length;i++){
+ botoes[i].onclick = function(){
+
+
+    for(let  j=0;j<botoes.length;j++){
+        botoes[j].classList.remove('ativa')
+    }
+    botoes[i].classlist.add("ativo");
+
+
+ }   
+    console.log(i);
+}
