79cb519d1091dd792963a9ce367e19f7c6e1280c
mahguerra08 <129458082+mahguerra08@users.noreply.github.com>
2024-08-08 22:34:04 -0300

    08/08

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 const botoes = document.querySelectorAll(".botao");
+const texto = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;i <botoes.length;i++){
- botoes[i].onclick = function(){
+    botoes[i].onclick=function(){
 
-    for(let j=0;j <botoes.length;j++){
-        botoes[j].classList.remove("ativo")
+        for(let i=0;i <botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            texto[j].classList.remove("ativo");
+        }
+    
+
+     botoes[i].classList.add("ativo");
+     texto[i].classList.add("ativo");
     }
-    botoes[i].classList.add("ativo");
-   
-   }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
